da3ca1b3ee68ae9b6f03c35965ebc3efca20f06a
Denis Fedoseev <denis.fedoseev@gmail.com>
2017-06-02 12:47:21 +0300

    AE::io was chahged to AE::ReadLine::Gnu for CLI interface

--- srv/cli.pl
+++ srv/cli.pl
@@ -5,12 +5,14 @@ use v5.20;
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
+use AnyEvent::ReadLine::Gnu;
 
 use Getopt::Long qw(GetOptionsFromString);
 
 use Data::Dumper;
 
 my $handle;
+my $rl;
 
 my $server_addr = '127.0.0.1';
 my $server_port = 44243;
@@ -22,6 +24,65 @@ my $speed = 115200;
 my $cmd   = "";
 my @opts  = qw/port=s speed=i file=s
   /;
+
+
+# Connect to server
+tcp_connect(
+    $server_addr,
+    $server_port,
+    sub {
+        my ($fh) = @_
+          or die "Unable to connect: $!";
+
+        $handle = AnyEvent::Handle->new(
+            fh      => $fh,
+            poll    => 'r',
+            on_read => sub {
+                my ($self) = @_;
+                $handle->push_read(
+                    json => sub {
+                        my ( $handle, $data ) = @_;
+                        $rl->print( Dumper($data));
+                    }
+                );
+
+            },
+            on_eof => sub {
+                my ($hdl) = @_;
+                $rl->print( "Connecton to server was closed.\n");
+                $hdl->destroy();
+                exit 0;
+            },
+            on_error => sub {
+                my $hdl = shift;
+                $rl->print( "Lost connecton to server.\n");
+                $hdl->destroy();
+                exit 1;
+            },
+        );
+
+        $handle->push_write( json => { command => "status" } );
+    }
+);
+
+# now initialise readline
+$rl = new AnyEvent::ReadLine::Gnu prompt => "cmd> ", on_line => sub {
+    my $line = shift;
+
+    if ( $line =~ m/^[G|M|T].*/ ) {
+        $handle->push_write(
+            json => { command => 'send', value => $line } );
+    }
+    else {
+        my $input = parse_input($line);
+        $handle->push_write( json =>
+              { command => $input->{'command'}, %{ $input->{'options'} } }
+        );
+    }    
+};
+
+$cv->recv;
+
 
 sub parse_input {
     my $input_line = shift;
@@ -39,64 +100,3 @@ sub parse_input {
     return { command => $cmd, options => $mopts };
 
 }
-
-tcp_connect(
-    $server_addr,
-    $server_port,
-    sub {
-        my ($fh) = @_
-          or die "Unable to connect: $!";
-
-        $handle = AnyEvent::Handle->new(
-            fh      => $fh,
-            poll    => 'r',
-            on_read => sub {
-                my ($self) = @_;
-                $handle->push_read(
-                    json => sub {
-                        my ( $handle, $data ) = @_;
-                        say Dumper($data);
-                    }
-                );
-
-            },
-            on_eof => sub {
-                my ($hdl) = @_;
-                print "Connecton to server was closed.";
-                $hdl->destroy();
-                exit 0;
-            },
-            on_error => sub {
-                my $hdl = shift;
-                print "Lost connecton to server.";
-                $hdl->destroy();
-                exit 1;
-            },
-        );
-
-        $handle->push_write( json => { command => "master_status" } );
-    }
-);
-
-my $wait_for_input = AnyEvent->io(
-    fh   => \*STDIN,    # which file handle to check
-    poll => "r",        # which event to wait for ("r"ead data)
-    cb   => sub {       # what callback to execute
-        my $line = <STDIN>;    # read it
-
-        chomp $line;
-        if ( $line =~ m/^[G|M|T].*/ ) {
-            $handle->push_write(
-                json => { command => 'send', value => $line } );
-        }
-        else {
-            my $input = parse_input($line);
-            $handle->push_write( json =>
-                  { command => $input->{'command'}, %{ $input->{'options'} } }
-            );
-        }
-
-    }
-);
-
-$cv->recv;
